ebc7ad427deefb4923ac277538a7265fcbaa75ff
xiatian <xt@zhoupengdeMacBook-Air.local>
2017-05-23 15:03:04 +0800

    first commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by xiatian on 2017/5/23.
+//  Copyright © 2017年 润阳贷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
